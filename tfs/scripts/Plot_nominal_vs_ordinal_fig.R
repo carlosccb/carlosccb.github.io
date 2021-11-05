892838d77ccab13213faa80ca7aaa03e7bdacf6a
carlosccb <carlos.cb.94@gmail.com>
2021-11-05 12:15:55 +0100

    Added point size, added shape in aes

--- tfs/scripts/Plot_nominal_vs_ordinal_fig.R
+++ tfs/scripts/Plot_nominal_vs_ordinal_fig.R
@@ -18,18 +18,20 @@ plot_theme <- function() {
 }
 
 # Plot with 3 classes
-ggplot(df_data, aes(x=x, y=y, color=target)) +
-  geom_point() +
-  theme_classic() +
+# TODO: use one single legend with shape and color
+ggplot(df_data, aes(x=x, y=y, color=target, shape=target)) +
+  geom_point(size=2.5) +
+  theme_classic() + # TODO: El causante de que salgan dos leyendas es theme_classic ¿?
   plot_theme() +
   labs(color="Degree")
 ggsave(file="ord_fig.png", device="png", path="~/Downloads/", scale=1.5, dpi=320)#, units="px")#, width=1290, height=1080)
 
 # Plot with 2 classes
+# TODO: use one single legend with shape and color
 df_data %>%
   mutate(target=replace(target, target==2, 1)) %>%
-  ggplot(aes(x=x, y=y, color=target)) +
-  geom_point() +
+  ggplot(aes(x=x, y=y, color=target, shape=target)) +
+  geom_point(size=2.5) +
   theme_classic() +
   plot_theme() +
   labs(color="Degree")

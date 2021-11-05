a41dc854dcf7d7dcb723efe68a6cf6a112859367
carlosccb <carlos.cb.94@gmail.com>
2021-11-05 03:01:15 +0100

    Changed text size

--- tfs/scripts/Plot_nominal_vs_ordinal_fig.R
+++ tfs/scripts/Plot_nominal_vs_ordinal_fig.R
@@ -12,6 +12,8 @@ plot_theme <- function() {
         axis.ticks.y = element_blank(),
         axis.text.x = element_blank(),
         axis.text.y = element_blank(),
+        legend.title = element_text(size=15),
+        legend.text = element_text(size=14),
   )
 }
 
